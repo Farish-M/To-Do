200501020046933d37d7992a3f3b38f0e0ed451f
Farish Mohamed Rozlan <farishmohr@gmail.com>
2021-07-06 16:58:32 +0800

    Fixed issues with the scrolling and appbar of both screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:productivity_todolist_schedule/screens/main/view/mainoverview_page.dart';
-import 'package:productivity_todolist_schedule/screens/main/widgets/todolist_builder.dart';
 
 void main() {
   runApp(ToDo());
@@ -12,9 +11,7 @@ class ToDo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: TodoListBuilder(),
-      ),
+      home: UserController(),
     );
   }
 }
