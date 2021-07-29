2fc6fc896c9403bb8c4ce20fb9c328ed48d92476
Farish Mohamed Rozlan <farishmohr@gmail.com>
2021-07-29 17:38:12 +0800

    Fully completed code, evaluation and commenting of the code

--- lib/screens/main/views/register.dart
+++ lib/screens/main/views/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productivity_todolist_schedule/screens/main/views/splash_page.dart';
 import 'package:productivity_todolist_schedule/screens/main/widgets/register_widget.dart';
 
 //Page that holds register widget for users to register
@@ -10,12 +11,19 @@ class RegisterPage extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text("Welcome!"),
+          leading: new IconButton(
+            icon: new Icon(Icons.arrow_back_ios, color: Colors.grey),
+            onPressed: () {
+              //Navigates the user to the previous page of NewToDo
+              Navigator.of(context).pop(_createRoute());
+            },
+          ),
         ),
         body: Container(
             child: Center(
           child: Container(
             //Parameters for the height and width of the container
-            //storing the SplashPageWidget
+            //storing the RegisterWidget
             height: 500,
             width: 250,
             child: RegisterWidget(),
@@ -23,4 +31,23 @@ class RegisterPage extends StatelessWidget {
           ),
         )));
   }
+}
+
+//Creates the route to the previous page that the user visted
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => SplashPage(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(0.0, 1.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+      //Sliding transition of the navigation
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
 }
